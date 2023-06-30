c8c93babc486f98e29df22e9ee0aa7868c9439f6
Honey <honiism@gmail.com>
2023-06-30 14:45:51 +0700

    update

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,29 @@
 <template>
-    <Landing :viewport="viewport"/>
+    <Landing v-if="isPortrait" :viewport="viewport"/>
+    <Footer v-if="isPortrait"/>
+    <Warning v-else/>
 </template>
 
-<style scoped lang="scss">
-    // Styling
-</style>
-
 <script setup>
-    import Landing from '../views/viewComps/Landing.vue';
+    import { ref } from "vue";
+
+    import Landing from "../views/viewComps/Landing.vue";
+    import Warning from "../components/LandscapeWarning.vue";
+    import Footer from "../components/Footer.vue"
 
     const props = defineProps({
         viewport: Object
+    });
+
+    const isPortrait = ref(true);
+
+    window.addEventListener("resize", () => {
+        const orientation = window.innerWidth > window.innerHeight ? "Landscape" : "Portrait";
+
+        if (orientation == "Landscape") {
+            isPortrait.value = false
+        } else if (orientation == "Portrait") {
+            isPortrait.value = true;
+        }
     });
 </script>
